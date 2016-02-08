68b2abd7e7aabc8a077c4c07836c8cb0074e2277
Aleksandar Timic <atimic@simscale.com>
2016-02-09 00:37:59 +0100

    Cleaning code a bit

--- wp-content/themes/fotoshooting/home.php
+++ wp-content/themes/fotoshooting/home.php
@@ -13,91 +13,56 @@
 
             <a href="<?php echo home_url(); ?>" class="logo"><img src="<?php echo get_template_directory_uri(); ?>/img/logo.jpg" alt="Foto-shooting" /></a>
 
-<!--            <ul class="menu-main">-->
-<!--                <li><a href="#">Portfolio</a></li>-->
-<!--                <li class="menu-item-angebote"><a href="#" >Angebote</a></li>-->
-<!--                <li><a href="#">Infos</a></li>-->
-<!--                <li><a href="#">Buchen</a></li>-->
-<!--                <li><a href="#">Blog</a></li>-->
-<!--                <li><a href="#">Kunden</a></li>-->
-<!--                <li><a href="#">Kontakt</a></li>-->
-<!--            </ul>-->
-
             <?php wp_nav_menu(array('theme_location' => 'main_menu', 'menu_class' => 'menu-main', 'link_before' => '', 'link_after' => '', 'depth'=> 1 )); ?>
 
             <?php
 
             $menu_name = 'main_menu'; // Get the nav menu based on $menu_name (same as 'theme_location' or 'menu' arg to wp_nav_menu)
 
-            if ( ( $locations = get_nav_menu_locations() ) && isset( $locations[ $menu_name ] ) ) {
-                $menu = wp_get_nav_menu_object( $locations[ $menu_name ] );
+            if (($locations = get_nav_menu_locations()) && isset($locations[$menu_name])) {
+                $menu = wp_get_nav_menu_object($locations[$menu_name]);
 
                 $menu_items = wp_get_nav_menu_items($menu->term_id);
 
                 //get all top menu items
                 $top_menu = array();
-                foreach ( (array) $menu_items as $key => $menu_item ) {
-                    if($menu_item->menu_item_parent=="0"){
-                        $top_menu[]=$menu_item;
+                foreach ((array)$menu_items as $key => $menu_item) {
+                    if ($menu_item->menu_item_parent == "0") {
+                        $top_menu[] = $menu_item;
                     }
                 }
 
                 //get all submenus for each top menu
-                $submenus= array(array());
-                foreach( (array) $top_menu as $top_menu_item) {
+                $submenus = array(array());
+                foreach ((array)$top_menu as $top_menu_item) {
                     foreach ((array)$menu_items as $key => $menu_item) {
-                        if($menu_item->menu_item_parent == $top_menu_item->ID){
-                            $submenus[$top_menu_item->ID][]=$menu_item;
+                        if ($menu_item->menu_item_parent == $top_menu_item->ID) {
+                            $submenus[$top_menu_item->ID][] = $menu_item;
                         }
                     }
                 }
-
-//                var_dump($submenus);
 
             }
-            ?>
 
-            <?php
-                echo '<div class="submenu-wrapper">';
+            echo '<div class="submenu-wrapper">';
 
-                foreach($submenus as $parentId => $submenu){
+            foreach ($submenus as $parentId => $submenu) {
 
-                    if($parentId!==0) {
+                if ($parentId !== 0) {
 
-                        echo '<ul class="submenu parent-id-' . $parentId . '">';
+                    echo '<ul class="submenu parent-id-' . $parentId . '">';
 
-                        foreach ($submenu as $key => $menu_item) {
-                            echo '<li><a href="' . $menu_item->url . '">' . $menu_item->title . '</a></li>';
-                        }
-
-                        echo '</ul>';
-
+                    foreach ($submenu as $key => $menu_item) {
+                        echo '<li><a href="' . $menu_item->url . '">' . $menu_item->title . '</a></li>';
                     }
-                }
 
-                echo '</div>';
+                    echo '</ul>';
+                }
+            }
+
+            echo '</div>';
             ?>
-<!--            <div class="submenu-wrapper">-->
-<!--                <ul class="submenu submenu-angebote">-->
-<!--                    <li><a href="#">Fotoshooting Frauen</a></li>-->
-<!--                    <li><a href="#">Baby & Family</a></li>-->
-<!--                    <li><a href="#">Foto Workshops</a></li>-->
-<!--                    <li><a href="#">Fotoshooting Männer</a></li>-->
-<!--                    <li><a href="#">Hochzeitsfotografie</a></li>-->
-<!---->
-<!--                    <li><a href="#">Fotostudio mieten</a></li>-->
-<!--                    <li><a href="#">Fotoshooting Models</a></li>-->
-<!--                    <li><a href="#">Business & Werbung</a></li>-->
-<!--                    <li><a href="#">Photobooth</a></li>-->
-<!--                    <li><a href="#">Bewerbungsfoto</a></li>-->
-<!---->
-<!--                    <li><a href="#">Geschenkgutschein</a></li>-->
-<!--                    <li><a href="#">Polterabend</a></li>-->
-<!--                    <li><a href="#">Partner & Friends</a></li>-->
-<!--                    <li><a href="#">Fotoshooting Aktionen</a></li>-->
-<!--                    <li><a href="#">Shooting Erweiterung</a></li>-->
-<!--                </ul>-->
-<!--            </div>-->
+
 
             <a href="#" class="btn-mobile-menu"></a>
 
